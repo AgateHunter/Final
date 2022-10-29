ac56d2b027c91230866c404497d3825dd121e335
Natalie Reilly <102992414+AgateHunter@users.noreply.github.com>
2022-10-29 11:50:40 -0700

    Add project files.

---
+++ Models/Transaction.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Final.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        //Category Id will be the Foreign Key for this table
+        public int CategoryId { get; set; }
+        public  Category Category {get; set;}
+
+        public int Time { get; set; }
+
+        [Column(TypeName = "nvarchar(MAX)")]
+        public string? Note { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
